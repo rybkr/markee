85457071cf58af517bc25ec4b0005e4430977657
Ryan Baker <rbaker@xtier.com>
2025-10-21 10:25:04 -0400

    Updata AST Node types to reflect two pass architecture

--- internal/parser/node.go
+++ internal/parser/node.go
@@ -5,13 +5,20 @@ type NodeType int
 
 const (
 	NodeDocument NodeType = iota
-	NodeParagraph
-	NodeHeader
-	NodeBlockquote
-	NodeCodeBlock
-	NodeText
-	NodeEmphasis
-	NodeStrong
+
+    NodeParagraph
+    NodeHeader
+    NodeCodeBlock
+    NodeBlockquote
+    NodeList
+    NodeListItem
+    NodeHorizontalRule
+
+    NodeText
+    NodeEmphasis
+    NodeStrong
+    NodeInlineCode
+    NodeLineBreak
 )
 
 type Node struct {
@@ -19,4 +26,15 @@ type Node struct {
 	Value    string
 	Level    int
 	Children []*Node
+}
+
+func NewNode(type NodeType) *Node {
+    return &Node{
+        Type:     type,
+        Children: []*Node{},
+    }
+}
+
+func (n *Node) AppendChild(child *Node) {
+    n.Children = append(n.Children, child)
 }
